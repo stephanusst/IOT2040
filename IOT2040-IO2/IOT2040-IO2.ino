69568939e6220bf0139b53fada9211ef5c65a587
stephanusst <stephanusst@gmail.com>
2025-06-19 15:44:37 +0700

    Commenting
    Adding explanation

--- IOT2040-IO2/IOT2040-IO2.ino
+++ IOT2040-IO2/IOT2040-IO2.ino
@@ -1,26 +1,13 @@
 /*
   Blink
-
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
+  Turns two LED on for one second, then off for one second, repeatedly.
+  Testing Arduino shield PIN
+  Size    83.771 bytes (0%) dari 
+      10.000.000 bytes.
+  
+  Modified 19 Junn 2025 by Stephanus 
 */
+
 #define DI0 12
 #define DI1 11
 #define DI2 10
@@ -39,19 +26,19 @@ void setup() {
   pinMode(DQ1, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);  
   
-  pinMode(DI0, OUTPUT);
-  pinMode(DI1, OUTPUT);
-  pinMode(DI2, OUTPUT);
-  pinMode(DI3, OUTPUT);
-  pinMode(DI4, OUTPUT);
+  pinMode(DI0, INPUT);
+  pinMode(DI1, INPUT);
+  pinMode(DI2, INPUT);
+  pinMode(DI3, INPUT);
+  pinMode(DI4, INPUT);
+  
   pinMode(0, OUTPUT);
   pinMode(1, OUTPUT);
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
+ 
   pinMode(5, OUTPUT);  
   pinMode(6, OUTPUT);  
-  pinMode(7, OUTPUT);  
-  pinMode(8, OUTPUT);  
 }
 
 // the loop function runs over and over again forever
@@ -59,6 +46,7 @@ void loop() {
   digitalWrite(DQ0, HIGH);   // turn the LED on (HIGH is the voltage level)
   digitalWrite(DQ1, HIGH);   // turn the LED on (HIGH is the voltage level)
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  
   digitalWrite(0, HIGH);   // turn the LED on (HIGH is the voltage level)
   digitalWrite(1, HIGH);   // turn the LED on (HIGH is the voltage level)
   digitalWrite(2, HIGH);   // turn the LED on (HIGH is the voltage level)

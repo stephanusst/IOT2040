fa2c4aaa1c1b0520816701e35217c93feae34fc2
Stephanus Surijadarma Tandjung, Ph.D <stephanusst@gmail.com>
2025-06-23 20:03:11 +0700

    Update mqtt_pub.py

--- mqtt/mqtt_pub.py
+++ mqtt/mqtt_pub.py
@@ -1,1 +1,12 @@
+import sys
 
+import paho.mqtt.client as paho
+
+client = paho.Client()
+
+if client.connect("emjsl.ddns.net", 1883, 60) != 0:
+    print("Couldn't connect to the mqtt broker")
+    sys.exit(1)
+
+client.publish("test", "Home", 0)
+client.disconnect()
